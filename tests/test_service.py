f79758ebfc48f38f47a9b652cb2c52a92e28b8e8
jagsdd <jagsdd@gmail.com>
2026-08-24 16:19:14 +0100

    Add service tests for create and update

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,5 +1,7 @@
 from unittest.mock import patch
-from app.service import create_server_service
+from app.service import create_server_service, update_server_service
+from app.validation import ALLOWED_STATUSES
+
 
 
 def test_create_server_service_valid():
@@ -25,7 +27,6 @@ def test_create_server_service_valid():
         assert response["server"]["name"] == "proxmox"
         assert response["server"]["status"] == "online"
 
-
 def test_create_server_service_invalid():
     data = {
         "status": "online"
@@ -37,3 +38,71 @@ def test_create_server_service_invalid():
         mock_create_server.assert_not_called()
         assert status == 400
         assert response["error"] == "Name is required"
+
+
+def test_update_server_service_valid():
+    server_id = 1
+
+    data = {
+        "status": "offline",
+    }
+
+    with patch("app.service.update_server") as mock_update_server:
+        mock_update_server.return_value = {
+            "id": 1,
+            "name": "proxmox",
+            "status": "offline"
+        }
+
+        response, status = update_server_service(server_id, data)
+
+        mock_update_server.assert_called_once_with(server_id, data)
+
+        assert status == 200
+        assert response["message"] == "server updated"
+
+        assert response["server"]["id"] == 1
+        assert response["server"]["name"] == "proxmox"
+        assert response["server"]["status"] == "offline"
+
+def test_update_server_service_no_data():
+    server_id = 1
+
+    data = None
+
+    with patch("app.service.update_server") as mock_update_server:
+        response, status = update_server_service(server_id, data)
+
+        mock_update_server.assert_not_called()
+        assert status == 400
+        assert response["error"] == "Invalid JSON"
+
+def test_update_server_service_invalid():
+    server_id = 1
+
+    data = {
+        "status": "broken",
+    }
+
+    with patch("app.service.update_server") as mock_update_server:
+        response, status = update_server_service(server_id, data)
+
+        mock_update_server.assert_not_called()
+        assert status == 400
+        assert response["error"] == f"Invalid status. Must be one of {sorted(ALLOWED_STATUSES)}"
+
+def test_update_server_service_not_found():
+    server_id = 999999
+
+    data = {
+        "status": "offline"
+    }
+
+    with patch("app.service.update_server") as mock_update_server:
+        mock_update_server.return_value = None
+
+        response, status = update_server_service(server_id, data)
+
+        mock_update_server.assert_called_once_with(server_id, data)
+        assert status == 404
+        assert response["error"] == "server not found"
